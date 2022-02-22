134c47c370d4fb2f9d853d1504c601f0bc09cece
Maureen <maureen.sturgeon@gmail.com>
2022-02-22 11:01:09 -0800

    create a migration to rename table and set flag

---
+++ pegasus/migrations/137_rename_user_storage_ids.rb
@@ -0,0 +1,17 @@
+Sequel.migration do
+  up do
+    user_storage_ids_pegasus = "#{CDO.pegasus_db_name}__user_storage_ids".to_sym
+    user_storage_ids_dashboard = "#{CDO.dashboard_db_name}__user_storage_ids".to_sym
+    rename_table(user_storage_ids_pegasus, user_storage_ids_dashboard)
+    DCDO.set('user_storage_ids_in_dashboard', true)
+    puts DCDO.get('user_storage_ids_in_dashboard', false)
+  end
+
+  down do
+    user_storage_ids_pegasus = "#{CDO.pegasus_db_name}__user_storage_ids".to_sym
+    user_storage_ids_dashboard = "#{CDO.dashboard_db_name}__user_storage_ids".to_sym
+    rename_table(user_storage_ids_dashboard, user_storage_ids_pegasus)
+    DCDO.set('user_storage_ids_in_dashboard', false)
+    puts DCDO.get('user_storage_ids_in_dashboard', false)
+  end
+end
